f46e8ff2584d903ddbde0e48a699b5788cee98b0
NILABH <nilavanjanchutia@gmail.com>
2022-09-09 18:31:06 +0530

    imge slider done

--- src/homeComponents/HomeFirst.js
+++ src/homeComponents/HomeFirst.js
@@ -1,44 +1,22 @@
-import React, { useState } from 'react';
-import { SliderData } from './SliderData';
-import { BsArrowRightCircle, BsArrowLeftCircle } from 'react-icons/bs';
+import React from 'react';
 
-const HomeFirst = () => {
-  const [current, setCurrent] = useState(0);
-  const length = SliderData.length;
+import ImgSlider from './ImgSlider';
 
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
+function HomeFirst ({SliderData})  {
 
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-
-  if (!Array.isArray(SliderData) || SliderData.length <= 0) {
-    return null;
-  }
 
   return (
-    <div className='invisible md:visible'>
-      {/* <div className='flex flex-cols-1 z-20'>
-        <h2>Your hair is our</h2>
-        <h1>Canvas!</h1>
-        <button>Book a Service</button>
-      </div> */}
-      <BsArrowLeftCircle className='left-arrow ' onClick={prevSlide} />
-      <BsArrowRightCircle className='right-arrow' onClick={nextSlide} />
-      {SliderData.map((slide, index) => {
-        return (
-          <div
-            className={index === current ? 'slide active' : 'slide'}
-            key={index}
-          >
-            {index === current && (
-              <img src={slide.image} alt="banners" className='' />
-            )}
-          </div>
-        );
-      })}
+    <div className=''>
+
+      {/* text and book Service button */}
+      <div className='grid justify-center mx-20 my-20 md:grid-cols-1 z-20 md:absolute lg:mx-48 lg:my-32 md:mx-32 md:my-20 '>
+        <h2 className='text-2xl md:text-3xl lg:text-4xl font-semibold'>Your hair is our</h2>
+        <h1 className='text-[#FF0000] text-4xl md:text-4xl lg:text-4xl font-semibold '>Canvas!</h1>
+        <div className='p-3 mt-5 bg-black w-48 rounded text-center text-white'>Book a Service</div>
+      </div>
+
+      {/* image slider c */}
+      <ImgSlider SliderData={SliderData}/>
     </div>
   );
 };
